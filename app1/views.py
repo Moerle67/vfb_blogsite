767eaa9d6ff740fb0f283a4483cc53299ddff708
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-05-06 14:07:10 +0200

    Templates

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
+
+def index(request):
+   print(request)
+   context = {
+            'name' : 'Klaus Apfelbaum',
+            'ort' : 'VFB-Ludwigsburg',
+            'datum' : datetime.now(),
+              }
+   return render(request, 'app1/start.html', context) 
